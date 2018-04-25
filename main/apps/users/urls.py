4387eff01c0ff7466f74318ca919e8a0dad0f4ad
ZombieAnomaly <kehran3@gmail.com>
2018-04-24 17:20:53 -0700

    fixed a bit with hex_board implementation

--- main/apps/users/urls.py
+++ main/apps/users/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
 	re_path('^lobby$', views.lobby),
 	re_path('^register$', views.register),
 	re_path('^login$', views.login),
+	re_path('^logout$', views.logout),
 	re_path('^username_validation/(?P<username>.*)$', views.check_username),
 	re_path('^email_validation/(?P<email>.*)$', views.check_email),
-	re_path('^game/(?P<game_id>\d+)$', views.get_game_data)
+	re_path('^game/(?P<game_id>\d+)$', views.get_game_data),
+	
 ]
