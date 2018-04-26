620a7ddab085b3c05dade378e066c27d4622ae14
Brian Spain <cloud@Brians-MacBook-Pro-2.local>
2018-04-25 22:57:38 -0700

    Fix game links in lobby.

--- main/apps/users/static/js/lobby_scripts.js
+++ main/apps/users/static/js/lobby_scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
+	updateGames()
+
+	$("#quickplay_4").click(function(){
+		$.get( "/join_public_game/4", function(res) {
+  			updateGames()
+		});
+	})
+
+	$("#quickplay_8").click(function(){
+		$.get( "/join_public_game/8", function(res) {
+  			updateGames()
+		});
+	})
+})
+
+function updateGames(){
 	$.getJSON( "/user/"+$("#user_id").val(), function(res) {
+  		$("#player_games").html("")
   		for(key in res['games']){
   			$("#player_games").append(
   				 "<div class='row'><div class='col-sm-8'><a href='/joingame/"+key+"'>"+res['games'][key]['name']+"</a></div><div class='col-sm-2'>"+ res['games'][key]['turn'] +
@@ -7,16 +24,4 @@ $(document).ready(function(){
   				)
   		}
 	});
-
-	$("#quickplay_4").click(function(){
-		$.get( "/join_public_game/4", function(res) {
-  			alert(res)
-		});
-	})
-
-	$("#quickplay_8").click(function(){
-		$.get( "/join_public_game/8", function(res) {
-  			alert(res)
-		});
-	})
-})
+}
